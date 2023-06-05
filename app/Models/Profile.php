295a00e82d0c384bdfda92c67e10c036f8585765
Thiago Silva <thiagosilva@p1it.com.br>
2023-06-04 22:46:59 -0300

    Criação de ACLs

---
+++ app/Models/Profile.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Profile extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name','description'];
+
+
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class);
+    }
+
+    public function permissionsAvailable()
+    {
+        $permissions = Permission::whereNotIn('id',function ($query){
+            $query->select('permission_profile.permission_id');
+            $query->from('permission_profile');
+            $query->whereRaw("permission_profile.profile_id={$this->id}");
+        })
+            ->get();
+
+        return $permissions;
+    }
+}
