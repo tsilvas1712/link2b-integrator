6abade37707a8eb78906633be9d748e5c9d9b0da
Thiago Silva <thiagosilva@p1it.com.br>
2023-07-03 18:36:39 -0300

    Bloqueio do Sync Schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@
     protected function schedule(Schedule $schedule): void
     {
       // $schedule->command('inspire')->hourly();
-      $schedule->command('datasys:sync')->dailyAt('10:30');
+      //$schedule->command('datasys:sync')->dailyAt('10:30');
       $schedule->command('link2bot:send')->dailyAt('12:30');
     }
 
