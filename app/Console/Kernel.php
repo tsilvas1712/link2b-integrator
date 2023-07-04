25f0a78ae0df974db8bd5e2f1107ae5a1e7269a4
Thiago Silva <thiagosilva@p1it.com.br>
2023-07-04 20:15:37 -0300

    Pausa no serviço de automação

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,31 +1,29 @@
 <?php
 
-  namespace App\Console;
+namespace App\Console;
 
-  use Illuminate\Console\Scheduling\Schedule;
-  use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
-  class Kernel extends ConsoleKernel
+class Kernel extends ConsoleKernel
+{
+  /**
+   * Define the application's command schedule.
+   */
+  protected function schedule(Schedule $schedule): void
   {
-    /**
-     * Define the application's command schedule.
-     */
-    protected function schedule(Schedule $schedule): void
-    {
-      // $schedule->command('inspire')->hourly();
-      //$schedule->command('datasys:sync')->dailyAt('10:30');
-      $schedule->command('link2bot:send')->dailyAt('12:30');
-    }
-
-    /**
-     * Register the commands for the application.
-     */
-    protected function commands(): void
-    {
-      $this->load(__DIR__ . '/Commands');
-
-      require base_path('routes/console.php');
-    }
-
-
+    // $schedule->command('inspire')->hourly();
+    //$schedule->command('datasys:sync')->dailyAt('10:30');
+    //$schedule->command('link2bot:send')->dailyAt('12:30');
   }
+
+  /**
+   * Register the commands for the application.
+   */
+  protected function commands(): void
+  {
+    $this->load(__DIR__ . '/Commands');
+
+    require base_path('routes/console.php');
+  }
+}
