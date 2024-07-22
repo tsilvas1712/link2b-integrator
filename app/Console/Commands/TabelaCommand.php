4944af4b0b614f768d67f5ed50500fedf10324f9
Thiago Silva <adsl.thiago@gmail.com>
2024-07-22 09:59:38 -0300

    ATAUALIZAÇÃO MENSAGENS

---
+++ app/Console/Commands/TabelaCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Campaign;
+use App\Models\Datasys;
+use App\Models\Sale;
+use App\Repository\DatasysRepository;
+use App\Repository\SaleRepository;
+use App\Services\DatasysService;
+use Illuminate\Console\Command;
+
+class TabelaCommand extends Command
+{
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'datasys:tabela';
+
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'Sincronizar todos os dados da tabela Datays para envio de mensagens';
+
+  /**
+   * Execute the console command.
+   */
+  public function handle()
+  {
+    $campaigns = Campaign::where('active', true)->get();
+    $datasys = new Datasys();
+    $sale = new Sale();
+
+    foreach ($campaigns as $campaign) {
+      $repositorySale = new SaleRepository($sale);
+      $repositoryDatasys = new DatasysRepository($datasys);
+      $dataService = new DatasysService($repositorySale, $repositoryDatasys);
+
+      $datasysToken = $campaign->token_customer;
+      $datasysUrl = $campaign->endpoint_customer;
+      $tenant_id = $campaign->tenant_id;
+
+
+      if ($campaign->type == 'API') {
+        $dataService->sendDatasys($tenant_id, $campaign->id, $campaign->sales_modalities);
+      }
+    }
+  }
+}
--- app/Http/Controllers/Integrador/SaleController.php
+++ app/Http/Controllers/Integrador/SaleController.php
@@ -1,116 +1,121 @@
 <?php
 
-  namespace App\Http\Controllers\Integrador;
+namespace App\Http\Controllers\Integrador;
 
-  use App\Http\Controllers\Controller;
-  use App\Models\Sale;
-  use Illuminate\Http\Request;
-  use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use App\Models\Sale;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
-  class SaleController extends Controller
+class SaleController extends Controller
+{
+  protected $repository;
+
+  public function __construct(Sale $sale)
   {
-    protected $repository;
-
-    public function __construct(Sale $sale)
-    {
-      $this->repository = $sale;
-      $this->middleware(['can:Mensagens']);
-    }
-
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-      $user = auth()->user();
-
-      if ($user->is_admin) {
-        $sales = $this->repository->latest()->paginate(10);
-
-        return view('Integrador.Sales.index', compact('sales'));
-      }
-      $sales = DB::table('sales')
-        ->leftJoin('campaigns', 'sales.campaign_id', '=', 'campaigns.id')
-        ->leftJoin('tenants', 'campaigns.tenant_id', '=', 'tenants.id')
-        ->where('tenants.id', $user->tenant_id)
-        ->orderBy('sales.data_pedido', 'DESC')
-        ->paginate(10);
-
-
-      return view('Integrador.Sales.index', compact('sales'));
-    }
-
-    public function indexCampaign($campaign)
-    {
-      $sales = DB::table('sales')
-        ->leftJoin('campaigns', 'sales.campaign_id', '=', 'campaigns.id')
-        ->leftJoin('tenants', 'campaigns.tenant_id', '=', 'tenants.id')
-        ->where('campaign_id', $campaign)
-        ->orderBy('sales.createD_at', 'DESC')
-        ->paginate(10);
-
-
-      return view('Integrador.Sales.index', compact('sales'));
-    }
-
-    public function indexStatus(string $status)
-    {
-      $sales = DB::table('sales')
-        ->leftJoin('campaigns', 'sales.campaign_id', '=', 'campaigns.id')
-        ->leftJoin('tenants', 'campaigns.tenant_id', '=', 'tenants.id')
-        ->where('status', $status)
-        ->orderBy('sales.createD_at', 'DESC')
-        ->paginate(10);
-
-
-      return view('Integrador.Sales.index', compact('sales'));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-      //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-      //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-      //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-      //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-      //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-      //
-    }
+    $this->repository = $sale;
+    $this->middleware(['can:Mensagens']);
   }
+
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    $user = auth()->user();
+
+    if ($user->is_admin) {
+      $sales = $this->repository->latest()->paginate(10);
+
+      return view('Integrador.Sales.index', compact('sales'));
+    }
+
+    $sales = DB::table('sales')
+      ->leftJoin('campaigns', 'sales.campaign_id', '=', 'campaigns.id')
+      ->leftJoin('tenants', 'campaigns.tenant_id', '=', 'tenants.id')
+      ->where('tenants.id', $user->tenant_id)
+      ->orderBy('sales.data_pedido', 'DESC')
+      ->paginate(10);
+
+
+    return view('Integrador.Sales.index', compact('sales'));
+  }
+
+  public function indexCampaign($campaign)
+  {
+    $sales = DB::table('sales')
+      ->leftJoin('campaigns', 'sales.campaign_id', '=', 'campaigns.id')
+      ->leftJoin('tenants', 'campaigns.tenant_id', '=', 'tenants.id')
+      ->where('campaign_id', $campaign)
+      ->orderBy('sales.createD_at', 'DESC')
+      ->paginate(10);
+
+
+    return view('Integrador.Sales.index', compact('sales'));
+  }
+
+  public function indexStatus(string $status)
+  {
+    $user = auth()->user();
+
+    $sales = DB::table('sales')
+      ->leftJoin('campaigns', 'sales.campaign_id', '=', 'campaigns.id')
+      ->leftJoin('tenants', 'campaigns.tenant_id', '=', 'tenants.id')
+      ->where('tenants.id', $user->tenant_id)
+      ->where('status', $status)
+
+      ->orderBy('sales.createD_at', 'DESC')
+      ->paginate(10);
+
+
+    return view('Integrador.Sales.index', compact('sales'));
+  }
+
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+    //
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request)
+  {
+    //
+  }
+
+  /**
+   * Display the specified resource.
+   */
+  public function show(string $id)
+  {
+    //
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(string $id)
+  {
+    //
+  }
+
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request, string $id)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(string $id)
+  {
+    //
+  }
+}
